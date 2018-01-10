163b34533de82d71ca0697295345bbeee8bcbd45
MaxSandi <maxsandi.work@gmail.com>
2018-01-10 15:56:04 +0200

    First Commit

---
+++ Model/NotesDictionary.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace NotesProject.Model
+{
+    public class NotesDictionary
+    {
+        private List<Note> notes;
+
+        public NotesDictionary()
+        {
+            notes = new List<Note>();
+
+            notes.Add(new Note("C", BitmapToBitmapSource(Properties.Resources.note_C), "до"));
+        }
+
+        public Note GetRandomNote()
+        {
+            try
+            {
+                return notes.ElementAt(GenerateRandom(0, notes.Count));
+            }
+            catch(IndexOutOfRangeException e)
+            {
+                MessageBox.Show(e.Message);
+                throw;
+            }
+        }
+
+        private BitmapSource BitmapToBitmapSource(Bitmap source)
+        {
+            return System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
+                          source.GetHbitmap(),
+                          IntPtr.Zero,
+                          Int32Rect.Empty,
+                          BitmapSizeOptions.FromEmptyOptions());
+        }
+        private int GenerateRandom(int min, int max)
+        {
+            var seed = Convert.ToInt32(Regex.Match(Guid.NewGuid().ToString(), @"\d+").Value);
+            return new Random(seed).Next(min, max);
+        }
+    }
+}
---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,48 @@
+﻿using GalaSoft.MvvmLight;
+using NotesProject.Model;
+using System.Windows.Media.Imaging;
+
+namespace NotesProject.ViewModel
+{
+
+    public class MainViewModel : ViewModelBase
+    {
+        private NotesDictionary _notesDictionary;
+
+        private string _textNote;
+        public string TextNote
+        {
+            get { return _textNote; }
+            set
+            {
+                if(_textNote != value)
+                {
+                    _textNote = value;
+                    RaisePropertyChanged("TextNote");
+                }
+            }
+        }
+        private BitmapSource _imageNote;
+        public BitmapSource ImageNote
+        {
+            get { return _imageNote; }
+            set
+            {
+                if(_imageNote != value)
+                {
+                    _imageNote = value;
+                    RaisePropertyChanged("ImageNote");
+                }
+            }
+        }
+
+        public MainViewModel(NotesDictionary notesDictionary)
+        {
+            _notesDictionary = notesDictionary;
+
+            Note note = _notesDictionary.GetRandomNote();
+            TextNote = "TEST";
+            ImageNote = note.Image;
+        }
+    }
+}
